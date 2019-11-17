a1eb273f516ae774a006dc81ee2ef9e5e941fc0a
jantopolovec <54882479+jantopolovec@users.noreply.github.com>
2019-11-17 20:08:36 +0100

    vprasanja, vprasanja edit/delete,

---
+++ vprasanja.js
@@ -0,0 +1,16 @@
+function izbrisi()
+{
+    Swal.fire(
+        'Uspešno',
+        'Izbrisali ste vprašanje!',
+        'success'
+      )
+}
+function izbrisi2()
+{
+    Swal.fire(
+        'Uspešno',
+        'Izbrisali ste rezultat!',
+        'success'
+      )
+}
